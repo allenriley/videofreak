26e9ea4cf3cf41edbe302caf72a548bc42a4a7f8
markkleeb <kleeb930@gmail.com>
2017-07-05 23:37:18 -0400

    title screen

---
+++ Assets/Scripts/PlayMovie.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PlayMovie : MonoBehaviour {
+
+	public KeyCode fire;
+
+	// Use this for initialization
+
+	void Start () {
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (fire)) {
+
+			SceneManager.LoadScene ("space2");
+
+
+		}
+	}
+}
+
+
+
